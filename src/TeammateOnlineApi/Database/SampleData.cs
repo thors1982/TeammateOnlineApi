71b7edcc0cc1b3062b0f5f3c75cdf90272fc4d3e
Nick Stark <thors1982@gmail.com>
2016-05-06 23:45:21 -0400

    Adding more seed and sample data

--- src/TeammateOnlineApi/Database/SampleData.cs
+++ src/TeammateOnlineApi/Database/SampleData.cs
@@ -14,8 +14,14 @@ namespace TeammateOnlineApi.Database
             if (!context.UserProfiles.Any())
             {
                 var ironman = context.UserProfiles.Add(new UserProfile { FirstName = "Tony", LastName = "Stark", EmailAddress = "tony.stark@ironman.com" });
+                var ironmanXbox = context.GameAccounts.Add(new GameAccount { UserName = "IronmanXbox", UserProfileId = ironman.Entity.Id, GamePlatformId = 1 });
+                var ironmanPlaystation = context.GameAccounts.Add(new GameAccount { UserName = "IronmanPlaystation", UserProfileId = ironman.Entity.Id, GamePlatformId = 2 });
+                var ironmanSteam = context.GameAccounts.Add(new GameAccount { UserName = "IronmanSteam", UserProfileId = ironman.Entity.Id, GamePlatformId = 3 });
+
                 var captainamerica = context.UserProfiles.Add(new UserProfile { FirstName = "Steve", LastName = "Rogers", EmailAddress = "steve.rogers@captainamerica.com" });
+
                 var hulk = context.UserProfiles.Add(new UserProfile { FirstName = "Bruce", LastName = "Banner", EmailAddress = "bruce.banner@hulk.com" });
+
                 var batman = context.UserProfiles.Add(new UserProfile { FirstName = "Bruce", LastName = "Wayne", EmailAddress = "brace.wayne@batman.com" });
 
                 context.SaveChanges();
