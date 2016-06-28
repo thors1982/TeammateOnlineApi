c2fc497b10db109bb1195c0541d4b7d981fe543c
Nick Stark <thors1982@gmail.com>
2016-06-27 21:52:38 -0400

    Remove unnecessary using

--- src/TeammateOnlineApi/Startup.cs
+++ src/TeammateOnlineApi/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
