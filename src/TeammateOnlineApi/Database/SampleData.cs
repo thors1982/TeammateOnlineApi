e0413bd5663ceaf584f914b8cc405cd3c9819326
Nick Stark <thors1982@gmail.com>
2016-08-06 17:57:15 -0400

    Improving sample data

--- src/TeammateOnlineApi/Database/SampleData.cs
+++ src/TeammateOnlineApi/Database/SampleData.cs
@@ -13,16 +13,34 @@ namespace TeammateOnlineApi.Database
 
             if (!context.UserProfiles.Any())
             {
+                // Some default user profiles
                 var ironman = context.UserProfiles.Add(new UserProfile { FirstName = "Tony", LastName = "Stark", EmailAddress = "tony.stark@ironman.com" });
+                var captainAmerica = context.UserProfiles.Add(new UserProfile { FirstName = "Steve", LastName = "Rogers", EmailAddress = "steve.rogers@captainamerica.com" });
+                var hulk = context.UserProfiles.Add(new UserProfile { FirstName = "Bruce", LastName = "Banner", EmailAddress = "bruce.banner@hulk.com" });
+                var batman = context.UserProfiles.Add(new UserProfile { FirstName = "Bruce", LastName = "Wayne", EmailAddress = "brace.wayne@batman.com" });
+
+                context.SaveChanges();
+
+                // Some default game accounts
                 var ironmanXbox = context.GameAccounts.Add(new GameAccount { UserName = "IronmanXbox", UserProfileId = ironman.Entity.Id, GamePlatformId = 1 });
                 var ironmanPlaystation = context.GameAccounts.Add(new GameAccount { UserName = "IronmanPlaystation", UserProfileId = ironman.Entity.Id, GamePlatformId = 2 });
                 var ironmanSteam = context.GameAccounts.Add(new GameAccount { UserName = "IronmanSteam", UserProfileId = ironman.Entity.Id, GamePlatformId = 3 });
 
-                var captainamerica = context.UserProfiles.Add(new UserProfile { FirstName = "Steve", LastName = "Rogers", EmailAddress = "steve.rogers@captainamerica.com" });
+                var captainAmericaXbox = context.GameAccounts.Add(new GameAccount { UserName = "CaptainAmericaXbox", UserProfileId = captainAmerica.Entity.Id, GamePlatformId = 1 });
+                var captainAmericaPlaystation = context.GameAccounts.Add(new GameAccount { UserName = "CaptainAmericaPlaystation", UserProfileId = captainAmerica.Entity.Id, GamePlatformId = 2 });
+                var captainAmericaSteam = context.GameAccounts.Add(new GameAccount { UserName = "CaptainAmericaSteam", UserProfileId = captainAmerica.Entity.Id, GamePlatformId = 3 });
 
-                var hulk = context.UserProfiles.Add(new UserProfile { FirstName = "Bruce", LastName = "Banner", EmailAddress = "bruce.banner@hulk.com" });
+                var hulkXbox = context.GameAccounts.Add(new GameAccount { UserName = "HulkXbox", UserProfileId = hulk.Entity.Id, GamePlatformId = 1 });
+                var hulkPlaystation = context.GameAccounts.Add(new GameAccount { UserName = "HulkPlaystation", UserProfileId = hulk.Entity.Id, GamePlatformId = 2 });
+                var hulkSteam = context.GameAccounts.Add(new GameAccount { UserName = "HulkSteam", UserProfileId = hulk.Entity.Id, GamePlatformId = 3 });
 
-                var batman = context.UserProfiles.Add(new UserProfile { FirstName = "Bruce", LastName = "Wayne", EmailAddress = "brace.wayne@batman.com" });
+                var batmanXbox = context.GameAccounts.Add(new GameAccount { UserName = "BatmanXbox", UserProfileId = batman.Entity.Id, GamePlatformId = 1 });
+                var batmanPlaystation = context.GameAccounts.Add(new GameAccount { UserName = "BatmanPlaystation", UserProfileId = batman.Entity.Id, GamePlatformId = 2 });
+                var batmanSteam = context.GameAccounts.Add(new GameAccount { UserName = "BatmanSteam", UserProfileId = batman.Entity.Id, GamePlatformId = 3 });
+
+                // Some default friends
+                var ironmansFriends = context.Friends.Add(new Friend { UserProfileId = ironman.Entity.Id, FriendUserProfileId = captainAmerica.Entity.Id });
+                var captainAmericaFriends = context.Friends.Add(new Friend { UserProfileId = captainAmerica.Entity.Id, FriendUserProfileId = ironman.Entity.Id });
 
                 context.SaveChanges();
             }
