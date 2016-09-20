3a860a0274340c1e57cbc41e174a6f9319ce70a8
Nick Stark <thors1982@gmail.com>
2016-09-19 22:57:44 -0400

    Add authorization helper method

---
+++ src/TeammateOnlineApi/Filters/AuthorizationFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using TeammateOnlineApi.Database.Repositories;
+
+namespace TeammateOnlineApi.Filters
+{
+    public class AuthorizationFilter
+    {
+        public bool IsUserAuthorizedForUser(ClaimsPrincipal currentUser, int userId)
+        {
+            var currentUserId = currentUser.Claims.FirstOrDefault(x => x.Type == "sub").Value;
+
+            return int.Parse(currentUserId) == userId;
+        }
+
+        public bool IsUserAuthorizedForFriend(ClaimsPrincipal currentUser, int friendId, IFriendRepository friendRepository)
+        {
+            var currentUserId = currentUser.Claims.FirstOrDefault(x => x.Type == "sub").Value;
+
+            return friendRepository.IsUserAFriend(int.Parse(currentUserId), friendId);
+        }
+
+        public bool IsUserAuthorizedForUserOrFriend(ClaimsPrincipal currentUser, int otherUserId, IFriendRepository friendRepository)
+        {
+            return IsUserAuthorizedForUser(currentUser, otherUserId) || IsUserAuthorizedForFriend(currentUser, otherUserId, friendRepository);
+        }
+    }
+}
