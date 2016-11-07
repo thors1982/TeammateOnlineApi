bf4a4667abee1ea6b549886f3c68398763a55ab5
Nick Stark <thors1982@gmail.com>
2016-11-06 22:59:09 -0500

    Adding unit test for processing time middleware

---
+++ src/TeammateOnlineApiTests/UnitTests/Middleware/ProcessingTimeMiddlewareTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using TeammateOnlineApi.Database;
+using TeammateOnlineApi.Database.Repositories;
+using Xunit;
+using TeammateOnlineApi.Models;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using TeammateOnlineApi;
+using System.Net.Http;
+using TeammateOnlineApi.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
+
+namespace TeammateOnlineApiTests.UnitTests.Middleware
+{
+    public class ProcessingTimeMiddlewareTests : IDisposable
+    {
+        private TestServer server;
+
+        private HttpClient client;
+
+        private int sleepTimer = 1000;
+
+        public ProcessingTimeMiddlewareTests()
+        {
+            var builder = new WebHostBuilder().Configure(
+                app => {
+                    app.UseProcessingTimeMiddleware();
+                    app.Run(async context =>
+                    {
+                        Thread.Sleep(sleepTimer);
+                        await context.Response.WriteAsync("Test response");
+                    });
+                });
+
+            server = new TestServer(builder);
+            client = server.CreateClient();
+        }
+
+        [Fact(DisplayName = "ProcessingTimeMiddleware - Check header greater than sleep")]
+        public async void AddProcessingTimeHeader_Returns200()
+        {
+            var requestMessage = new HttpRequestMessage(new HttpMethod("GET"), "/test-processing-time/");
+            var responseMessage = await client.SendAsync(requestMessage);
+            var processingTimeHeader = responseMessage.Headers.FirstOrDefault(h => h.Key == "X-ProcessingTime");
+
+            Assert.Equal("X-ProcessingTime", processingTimeHeader.Key);
+
+            var processingTimeValue = Int32.Parse(processingTimeHeader.Value.FirstOrDefault().Replace(" ms", ""));
+
+            Assert.True(processingTimeValue > sleepTimer);
+        }
+
+        public void Dispose()
+        {
+            server.Dispose();
+            client.Dispose();
+        }
+    }
+}
